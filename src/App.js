3e679fc579ab682961edb3bf6b589a69032db3fc
KubatovaTatyna <tatynakubatova@gmail.com>
2023-12-25 00:57:15 +0600

    sdfs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, ScrollRestoration } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Tour from "./pages/tour";
 
