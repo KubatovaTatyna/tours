b00469dd100804403e414d3a8f7bdada3b472db9
tatyna <75364411+KubatovaTatyna@users.noreply.github.com>
2024-01-27 13:53:38 +0600

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -26,7 +26,7 @@ const Home = () => {
         {/* <OurTeam  ourTeam={home.ourTeam}/> */}
         {/* <Hotels /> */}
         <div className="activites">
-            <h1>Gallery</h1>
+            <h1 className='text-center display-1 font-weight-bold'>Gallery</h1>
             <SliderComponent
                 section={home.gallery} />
         </div>
